1f56e593e1fd7641c58f892b7f6e065eb583639a
Nathaniel Bibler <git@nathanielbibler.com>
2010-03-05 11:16:12 -0500

    Switched to MadMimiMailer, added welcome email

--- app/models/blue_light_special_mailer.rb
+++ app/models/blue_light_special_mailer.rb
@@ -1,11 +1,20 @@
-class BlueLightSpecialMailer < ActionMailer::Base
+class BlueLightSpecialMailer < MadMimiMailer
 
-  def change_password(user)
+  def mimi_change_password(user)
     from       BlueLightSpecial.configuration.mailer_sender
     recipients user.email
     subject    I18n.t(:change_password,
                       :scope   => [:blue_light_special, :models, :blue_light_special_mailer],
                       :default => "Change your password")
+    body       :user => user
+  end
+  
+  def mimi_welcome(user)
+    from       BlueLightSpecial.configuration.mailer_sender
+    recipients user.email
+    subject    I18n.t(:welcome,
+                      :scope   => [:blue_light_special, :models, :blue_light_special_mailer],
+                      :default => "Welcome")
     body       :user => user
   end
 
