1399bac9a1c10b0a2db9563e5464792c9e4b1505
Nathaniel Bibler <git@nathanielbibler.com>
2010-03-05 11:30:03 -0500

    Added admin_user Factory

--- generators/blue_light_special/templates/factories.rb
+++ generators/blue_light_special/templates/factories.rb
@@ -7,3 +7,7 @@ Factory.define :user do |user|
   user.password              { "password" }
   user.password_confirmation { "password" }
 end
+
+Factory.define :admin_user, :parent => :user do |admin|
+  admin.role                 'admin'
+end
