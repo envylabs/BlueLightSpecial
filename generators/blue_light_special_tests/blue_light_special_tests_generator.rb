d22bea2e3d1a59e0aa092896640f9c6dbf000eb3
Nathaniel Bibler <git@nathanielbibler.com>
2010-03-05 13:19:14 -0500

    Force directory creation

--- generators/blue_light_special_tests/blue_light_special_tests_generator.rb
+++ generators/blue_light_special_tests/blue_light_special_tests_generator.rb
@@ -2,9 +2,8 @@ class BlueLightSpecialTestsGenerator < Rails::Generator::Base
 
   def manifest
     record do |m|
-      m.directory File.join("test", "unit")
-      m.directory File.join("test", "functional")
       m.directory File.join("test", "integration")
+      m.directory File.join("test", "facebooker_fixtures", "facebook.users.getInfo")
 
       ["test/integration/facebook_test.rb",
        "test/integration/impersonation_test.rb",
@@ -12,7 +11,7 @@ class BlueLightSpecialTestsGenerator < Rails::Generator::Base
        "test/integration/sign_out_test.rb",
        "test/integration/sign_up_test.rb",
        "test/integration/password_reset_test.rb",
-       "test/facebooker_fixtures/facebooker.users.getInfo/609e98c680f254540205acf931ae2963.xml"].each do |file|
+       "test/facebooker_fixtures/facebook.users.getInfo/609e98c680f254540205acf931ae2963.xml"].each do |file|
         m.file file, file
        end
       
