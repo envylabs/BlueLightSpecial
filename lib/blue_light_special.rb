1628cd0b79585fe194c1b1d77d3150b839ff89c3
Mark Kendall <mark@binarylotus.com>
2010-05-07 12:56:32 -0400

    Added Rails 3 Engine definition

--- lib/blue_light_special.rb
+++ lib/blue_light_special.rb
@@ -5,3 +5,5 @@ require 'blue_light_special/configuration'
 require 'blue_light_special/routes'
 require 'blue_light_special/authentication'
 require 'blue_light_special/user'
+
+require 'blue_light_special/engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3
---
+++ lib/blue_light_special/engine.rb
@@ -0,0 +1,8 @@
+require "blue_light_special"
+require "rails"
+
+module BlueLightSpecial
+ class Engine < Rails::Engine
+    engine_name :blue_light_special
+  end
+end
